7c6a0b53b40fcfbbfcfd6dea62eb8d1fc63de0bc
Maxiron794 <56217284+Maxiron794@users.noreply.github.com>
2021-11-28 22:55:48 +0200

    Update Program.cs

--- worldgenerator/Program.cs
+++ worldgenerator/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
 using System.IO;
 
 using SimplexNoise;
